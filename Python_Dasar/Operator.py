63d82e9e5ea25b9e4e20be9d037cbdd4bdebf136
syahrulUlum <sahrululum73@gmail.com>
2023-03-18 22:26:23 +0700

    Operator

---
+++ Python_Dasar/Operator.py
@@ -0,0 +1,17 @@
+angka_pertama = 10
+angka_kedua = 20
+
+# Penambahan
+print(angka_pertama + angka_kedua)
+
+# Pengurangan
+print(angka_pertama - angka_kedua)
+
+# Perkalian
+print(angka_pertama * angka_kedua)
+
+# Pembagian
+print(angka_pertama / angka_kedua)
+
+# Sisa bagi hasil / modulo
+print(angka_pertama % angka_kedua)
