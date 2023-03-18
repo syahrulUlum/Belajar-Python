c15746521576b15d36232d554dfd1b6ae3a57265
syahrulUlum <sahrululum73@gmail.com>
2023-03-18 22:22:30 +0700

    Variable dan Type Data

---
+++ Python_Dasar/Variable_TypeData.py
@@ -0,0 +1,57 @@
+# Variable tidak boleh dimulai dengan nomor
+# Variable tidak boleh ada spasi
+# Variable tidak boleh menggunakan simbol
+# Penamaan Variable untuk python umumnya menggunakan snake case (contoh_variable)
+
+# Deklarasi Variable
+angka_pertama = 10
+
+# Integer
+integer = 1
+print(integer)
+
+# Float
+float = 1.5
+print(float)
+
+# String
+string = "halo"
+print(string)
+
+# Boolean hanya berisi True atau False
+bool = True
+print(bool)
+
+# Di List, dan Tuple ada yang namanya index / nomor penempatan Value atau Isi
+# Index diawali dari nomor / angka 0
+# Pada contoh dibawah index 0 berisi angka 1, index 1 berisi "apa" dan seterusnya
+# Jika ingin memunculkan Value atau Isi pada index tertentu maka tinggal panggil seperti ini
+# nama_variable[index]
+
+# List ~ Value atau Isi dapat diubah
+list = [1, "apa", 1, "apa"]
+print(list)
+print(list[1])
+
+# Tuple ~ Value atau Isi tidak dapat diubah
+tuple = (1, "apa", 1, "apa")
+print(tuple)
+
+# Set ~ Value atau isi tidak dapat diubah tetapi bisa dikurangi atau ditambah
+# Nilai yang double tidak akan dimunculkan
+# Bersifat unorderable sehingga tidak berlaku index
+set = {"a", "b", "a"}
+print(set)
+
+# Dictionary
+# Mempunyai Key dan Value
+# Key harus menggunakan string
+# Jika ingin memanggil atau memunculkan atau mencetak nilai tertentu
+# Gunakanlah key
+
+dict = {
+    "nama": "aku",
+    "umur": 100
+}
+print(dict)
+print(dict["nama"])
